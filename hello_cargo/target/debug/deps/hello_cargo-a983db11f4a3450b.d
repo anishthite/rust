9e7948c4d5a19a91b82b11bd2b01e8b1a42556c4
Anish Thite <anishthite@gmail.com>
2020-05-12 16:08:45 -0400

    chapter 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-a983db11f4a3450b.d
@@ -0,0 +1,5 @@
+/home/anishthite/Workspace/rust/hello_cargo/target/debug/deps/hello_cargo-a983db11f4a3450b: src/main.rs
+
+/home/anishthite/Workspace/rust/hello_cargo/target/debug/deps/hello_cargo-a983db11f4a3450b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/anishthite/Workspace/rust/hello_cargo/target/debug/hello_cargo: /home/anishthite/Workspace/rust/hello_cargo/src/main.rs
